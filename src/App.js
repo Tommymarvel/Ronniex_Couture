2df141276ddf8171419492b51dd34e88deceedd5
Tommymarvel <ibironketomiwa4@gmail.com>
2023-02-28 17:53:24 +0100

    Add necessary configurations

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="font-gloock font-extrabold text-5xl"> Hello world </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase-config.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyBXBmzWuyCGninthKn5haDpBxZD5pkTwb4',
+  authDomain: 'react-firebase-b352b.firebaseapp.com',
+  projectId: 'react-firebase-b352b',
+  storageBucket: 'react-firebase-b352b.appspot.com',
+  messagingSenderId: '887905310732',
+  appId: '1:887905310732:web:fa521970485520a776f629',
+  measurementId: 'G-SNXB5W0M32',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
